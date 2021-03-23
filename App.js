c8867775f30241f7311d0774352a3ca6fa241d65
hijazi (mac m1ni) <hijazi@irekasoft.com>
2021-03-23 12:04:19 +0800

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,86 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react'
+import { View, Text, Button, Alert, Switch, TextInput, Image } from 'react-native'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      text: 'Back at 1pm ',
+
+    }
+
+  }  
+
+
+  render() {
+
+    let { text } = this.state;
+
+    return (
+      <View style={styles.container}>
+        <Text style={{fontSize: 150}} >{text}</Text>
+        <Button title="Hello" 
+
+          onPress={()=>{
+            console.log('hi');
+            
+            Alert.alert(text);
+
+          }}
+
+        />
+
+      <Switch
+        
+      />  
+
+      <TextInput
+        style={{
+          height: 40,
+          width: 300,
+          margin: 12,
+          borderWidth: 1,
+          padding: 12,
+          fontSize: 20,
+        }}
+        value={this.state.text}
+        onChangeText={
+          (text)=>{
+
+            this.setState({
+              text: text,
+            })
+
+          }
+        }
+        placeholder="useless placeholder"
+        keyboardType="numeric"
+      />
+
+      <Image
+          style={{width:140, height: 140}}
+          source={{uri: 'https://reactnative.dev/img/tiny_logo.png'}}
+        />
+
+      </View>
+    )
+
+  }
+
 }
 
-const styles = StyleSheet.create({
+const styles = {
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    flex: 1, 
     justifyContent: 'center',
-  },
-});
+    alignItems: 'center',
+    flexDirection:'column'
+  }
+}
+
+
+export default App;
+
+
